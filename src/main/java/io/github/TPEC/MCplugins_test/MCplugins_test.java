c911341e9f577fd465013c74504bef24ac7098a8
TPEC <489340274@qq.com>
2016-07-26 19:06:17 +0800

    build

---
+++ src/main/java/io/github/TPEC/MCplugins_test/MCplugins_test.java
@@ -0,0 +1,14 @@
+package io.github.TPEC.MCplugins_test;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCplugins_test extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		getLogger().info("On Enable");
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
